e4454765e0801aec56488e03e24e003f710c421f
Nuit <bhans@qq.com>
2026-01-28 00:26:01 +0800

    Initial Commit

---
+++ Wing/WingApp.swift
@@ -0,0 +1,32 @@
+//
+//  WingApp.swift
+//  Wing
+//
+//  Created by Hans on 2026/1/28.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct WingApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ WingTests/WingTests.swift
@@ -0,0 +1,17 @@
+//
+//  WingTests.swift
+//  WingTests
+//
+//  Created by Hans on 2026/1/28.
+//
+
+import Testing
+@testable import Wing
+
+struct WingTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
